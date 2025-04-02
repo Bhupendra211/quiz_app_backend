cd67a265d9d088c13c3d4f5baf6873d57373652c
Bhupendra Singh Bisht <59397470+Bhupendra211@users.noreply.github.com>
2025-04-02 10:36:52 +0530

    Update worker.js

--- src/utils/worker.js
+++ src/utils/worker.js
@@ -1,13 +1,20 @@
 import { Worker } from "bullmq";
 import StudentAttempt from "../models/studentAttempt.model.js";
 import { client } from "../config/redis.js";
+import dbConnection from "./dbConnection.js";
 
-const quizWorker = new Worker('quizSubmissionQueue', async (job) => {
+const connection= async ()=>{
+    await dbConnection();
+}
+
+const quizWorker = new Worker('quizQueue', async (job) => {
     try {
+
+        await connection();
         const { student_id, quiz_id, answers } = job.data;
 
+
         await StudentAttempt.insertMany([{ student_id, quiz_id, answers }]);
-        console.log(`Quiz submission processed for Student: ${student_id}, Quiz: ${quiz_id}`);
 
     } catch (error) {
         console.error("Quiz submission processing failed:", error);
